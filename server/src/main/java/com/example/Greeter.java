e26fe569847f9818e80ec8bbb5afad249ed09fd8
SoumayaHammami <52749788+SoumayaHammami@users.noreply.github.com>
2019-10-30 11:09:09 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,13 +1,22 @@
 package com.example;
 
-
-public class Greeter {
-
-  
+/**
+ *
+ * @author stagiaire
+ *
+ */
+public class Greeter { 
+  /**
+     * default constructeur.
+     */
   public Greeter() {
 
   }
-
+ /**
+     *
+     * @param someone someone
+     * @return String
+     */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
